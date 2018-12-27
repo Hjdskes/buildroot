8c3551e5c4f581a9b5d538f36335fca8710a4916
Jente Hidskes <hjdskes@gmail.com>
2018-12-27 16:38:33 +0100

    Add defconfig and board files for my RasPi 3B+ image
    See https://www.hjdskes.nl/projects/rpi-linux

---
+++ board/hjdskes/rpi3/post-image.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+cp board/hjdskes/${BOARD_NAME}/config.txt ${BINARIES_DIR}/config.txt
+cp board/hjdskes/${BOARD_NAME}/cmdline.txt ${BINARIES_DIR}/cmdline.txt
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage --rootpath "${TARGET_DIR}"     \
+	 --tmppath "${GENIMAGE_TMP}"    \
+	 --inputpath "${BINARIES_DIR}"  \
+	 --outputpath "${BINARIES_DIR}" \
+	 --config "${GENIMAGE_CFG}"
